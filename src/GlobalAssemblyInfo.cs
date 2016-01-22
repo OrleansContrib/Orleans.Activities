e4ec1172fff837a7f03fc690226a5c9f73793fd7
lmagyar <lmagyar@users.noreply.github.com>
2016-01-22 10:11:00 +0100

    Update assembly info
    Due to transfer to OrleansContrib.

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 
-[assembly: AssemblyCompany("https://github.com/lmagyar")]
-[assembly: AssemblyProduct("Orleans.Activities - https://github.com/lmagyar/Orleans.Activities")]
-[assembly: AssemblyCopyright("Copyright © https://github.com/lmagyar 2016")]
+[assembly: AssemblyCompany("https://github.com/OrleansContrib")]
+[assembly: AssemblyProduct("Orleans.Activities - https://github.com/OrleansContrib/Orleans.Activities")]
+[assembly: AssemblyCopyright("Copyright © https://github.com/OrleansContrib 2016")]
 
 [assembly: AssemblyVersion("0.2.0.0")]
 [assembly: AssemblyInformationalVersion("0.2.0-alpha")]
