c75187e4d13989593ef9f558a471ec979cfd2a4e
lmagyar <lmagyar@users.noreply.github.com>
2016-01-24 04:21:09 +0100

    Update version to v0.1.1

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("Orleans.Activities - https://github.com/OrleansContrib/Orleans.Activities")]
 [assembly: AssemblyCopyright("Copyright © https://github.com/OrleansContrib 2016")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyInformationalVersion("0.2.0-alpha")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.1")]
