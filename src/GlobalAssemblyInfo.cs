ee91db282050feea4a73b61292748a2ffa92afee
lmagyar <lmagyar@users.noreply.github.com>
2017-04-16 19:41:16 +0200

    Modify year in copyright message

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyCompany("OrleansContrib")]
 [assembly: AssemblyProduct("Orleans.Activities - https://github.com/OrleansContrib/Orleans.Activities")]
-[assembly: AssemblyCopyright("Copyright © OrleansContrib 2016")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017 OrleansContrib")]
 
 [assembly: AssemblyVersion("0.3.0")]
 [assembly: AssemblyFileVersion("0.3.0")]
