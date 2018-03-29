f20c8ef963ec1c412be83759f88c434ae1095dba
lmagyar <lmagyar@users.noreply.github.com>
2018-03-30 00:03:27 +0200

    inc version

--- Samples/src/GlobalAssemblyInfo.cs
+++ Samples/src/GlobalAssemblyInfo.cs
@@ -2,10 +2,10 @@
 
 [assembly: AssemblyCompany("OrleansContrib")]
 [assembly: AssemblyProduct("Orleans.Activities.Samples - https://github.com/OrleansContrib/Orleans.Activities")]
-[assembly: AssemblyCopyright("Copyright © 2016-2017 OrleansContrib")]
+[assembly: AssemblyCopyright("Copyright © 2016-2018 OrleansContrib")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.3.1")]
-[assembly: AssemblyFileVersion("0.3.1")]
-[assembly: AssemblyInformationalVersion("0.3.1")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
+[assembly: AssemblyInformationalVersion("0.4.0")]
